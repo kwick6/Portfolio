255b1191fe7874683ded30acadf4f2447b583119
SG <sy1840@gmail.com>
2013-10-08 13:41:27 -0700

    Adding new portfolio files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '0b535e3266af11155d42e8688e2c250dfeb68871794230b0e48044460d9893283e2ab6994bb7c28904f92fdf6c90ea92a9f6358e728ca8629c0ba6beee932ba0'
