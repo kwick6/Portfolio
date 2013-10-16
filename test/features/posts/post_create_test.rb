88309cc8d2271fe0914f5b1f807e33914dc0457f
SG <sy1840@gmail.com>
2013-10-16 16:17:51 -0700

    clean up tests

--- test/features/posts/post_create_test.rb
+++ test/features/posts/post_create_test.rb
@@ -4,18 +4,22 @@ feature "creating a post" do
   scenario "works with valid data" do
 
     #Given a signed in user
+
     sign_in_user
-    visit posts_path
+
+    click_link 'Blog'
+    click_link 'New Post'
+
     #Create a new post
-    click_on "New Post"
-    fill_in 'Title', with: posts(:cr).title
+
+    #fill_in 'Title', with: posts(:cr).title
     fill_in 'Content', with: posts(:cr).content
 
     # When I submit the form
     click_on 'Create Post'
 
     # Then I should see the post
-    page.text.must_include posts(:cr).title
+    page.text.must_include posts(:cr).content
 
     # And a success message
     page.text.must_include 'Post was successfully created'
