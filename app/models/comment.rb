254ade31875667032f8b63ec9b6a6d7dec8dad21
SG <sy1840@gmail.com>
2013-10-22 09:38:43 -0700

    add comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
+  #attr_accessible :approved, :author, :author_email, :author_url, :content, :referrer, :user_agent, :user_ip
   belongs_to :post
-  attr_accessible :approved, :author, :author_email, :author_url, :content, :referrer, :user_agent, :user_ip
+  validates :content,      presence: true
+  validates :author_email, presence: true
+  validates :author,       presence: true
 end
