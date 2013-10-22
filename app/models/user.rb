dbd1863092bc0ec7ddc465484726c782f031963a
SG <sy1840@gmail.com>
2013-10-22 15:30:28 -0700

    updated oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   where(auth.slice(:provider, :uid)).first_or_create do |user|
     user.provider = auth.provider
     user.uid = auth.uid
-    user.username = auth.info.nickname
+    user.name = auth.info.nickname
+    user.email = "#{user.name}-CHANGEME@twitter.example.com"
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022214517) do
+ActiveRecord::Schema.define(:version => 20131022222617) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20131022214517) do
     t.string   "role"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
